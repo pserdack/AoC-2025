423542a67d01de421151a0c2ba2a4068dce928bb
pserdack <paul.serdack@gmail.com>
2025-12-08 20:53:26 +0100

    Micha/ Add Implementation

--- Day02/csharp/Day02/Part1.cs
+++ Day02/csharp/Day02/Part1.cs
@@ -15,4 +15,35 @@ public class Part1
             .ToList();
     }
 
+    public bool IdIsValid(int id){
+        var length = id.ToString().Length;
+        if (length % 2 != 1) 
+        {
+            // Consider/Ignore: int.ToString().Substring(0, length/2) -> int.firstHalf() ... int.secondHalf()
+            if(id.FirstHalf() == id.SecondHalf())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+public static class IntExtension 
+{
+    extension(int input)
+    {
+        public string FirstHalf()
+        {
+            var inputString = input.ToString();
+            return inputString.Substring(0, inputString.Length / 2);
+        }
+
+        public string SecondHalf()
+        {
+            var inputString = input.ToString();
+            return inputString.Substring(inputString.Length / 2);
+        }
+    }
 }
