f64d4821323034105492b03a1134fb0582e03ec0
pserdack <paul.serdack@gmail.com>
2025-12-08 19:22:36 +0100

    Add Scaffolding

--- Day02/csharp/Day02/Program.cs
+++ Day02/csharp/Day02/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string fileName = "../test.txt";
+
+if (args.Length > 0 && args[0] == "paul")
+{
+    fileName = "../testPaul.txt";
+} else if (args.Length > 0 && args[0] == "Day02/micha")
+{
+    fileName = "testMicha.txt";
+}
+
+Console.WriteLine(File.ReadAllText(fileName));
